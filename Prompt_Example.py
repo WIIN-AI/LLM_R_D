f6b9ff5bae83eff2464f06cf0b31147f9be45991
Vinaykumar Puppala <37238369+WIIN-AI@users.noreply.github.com>
2023-09-11 20:09:16 +0530

    Create Prompt_Example.py

---
+++ Prompt_Example.py
@@ -0,0 +1,45 @@
+## Default LLaMA-2 prompt style
+B_INST, E_INST = "[INST]", "[/INST]"
+B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+DEFAULT_SYSTEM_PROMPT = """\
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
+
+def get_prompt(instruction, new_system_prompt=DEFAULT_SYSTEM_PROMPT ):
+    SYSTEM_PROMPT = B_SYS + new_system_prompt + E_SYS
+    prompt_template =  B_INST + SYSTEM_PROMPT + instruction + E_INST
+    return prompt_template
+
+
+sys_prompt = """You are a helpful, respectful and honest assistant.
+Always answer as helpfully as possible using the context text provided. Your answers should only answer the question
+once and not have any text after the answer is done.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct.
+If you don't know the answer to a question, please don't share false information. """
+
+instruction = """CONTEXT:/n/n {context}/n
+
+Question: {question}"""
+get_prompt(instruction, sys_prompt)
+
+from langchain.prompts import PromptTemplate
+prompt_template = get_prompt(instruction, sys_prompt)
+
+llama_prompt = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+chain_type_kwargs = {"prompt": llama_prompt}
+
+from langchain.schema import prompt
+
+from langchain.schema import prompt
+# create the chain to answer questions
+qa = RetrievalQA.from_chain_type(llm=llm,
+                                       chain_type="stuff",
+                                       retriever=retriever,
+                                       chain_type_kwargs=chain_type_kwargs,
+                                       return_source_documents=True)
+
